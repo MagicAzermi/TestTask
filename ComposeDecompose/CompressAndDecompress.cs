14ba6f324f6ea713f9e0494ed1c90b5ef70680ab
Mach Rock <rockmach6@gmail.com>
2025-10-28 23:12:28 +0300

    Решение задания 1

---
+++ ComposeDecompose/CompressAndDecompress.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace ComposeDecompose
+{
+    public class CompressAndDecompress
+    {
+
+
+        public string Compress(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString)) return inputString;
+
+            StringBuilder _outputString = new StringBuilder();
+            int? _counter = 1;
+            char? _currentChar = inputString[0];
+
+            for (int _index = 0; _index < inputString.Length; _index++)
+            {
+                if(inputString[_index] != _currentChar)
+                {
+                    _outputString.Append(_currentChar);
+                    if (_counter > 1) _outputString.Append(_counter);
+                    _currentChar = inputString[_index];
+                    _counter = 1;
+                }
+                else
+                {
+                    _counter++;
+                }
+            }
+
+            _outputString.Append(_currentChar);
+            if (_counter > 1) _outputString.Append(_counter);
+
+            return _outputString.ToString();
+        }
+
+        public string Decompress(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString)) return inputString;
+
+            StringBuilder _outputString = new StringBuilder();
+            StringBuilder _letterCountString = new StringBuilder();
+            int _index = 0;
+
+
+            while (_index < inputString.Length)
+            {
+                char _currentChar = inputString[_index++];
+                _outputString.Append( _currentChar);
+
+                _letterCountString.Clear();
+                while (_index < inputString.Length && Char.IsDigit(inputString[_index]))
+                {
+                    _letterCountString.Append(inputString[_index]);
+                    _index++;
+                }
+
+                if(_letterCountString.ToString() != "")
+                {
+                    _outputString.Append(new string(_currentChar, Int32.Parse(_letterCountString.ToString()) - 1));
+                }
+            }
+
+            return _outputString.ToString();
+        }
+    }
+}
---
+++ ComposeDecompose/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposeDecompose
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var _newClass = new CompressAndDecompress();
+
+            Console.WriteLine("Строка:");
+            string? _str = Console.ReadLine();
+
+            _str = _newClass.Compress(_str);
+            Console.WriteLine(_str);
+
+            _str = _newClass.Decompress(_str);
+            Console.WriteLine(_str);
+
+        }
+    }
+}
